5c07a01f213a25bcae077c3ebd4c2d7dfa05cd56
ShovalWeinstock <92527489+ShovalWeinstock@users.noreply.github.com>
2024-01-26 15:39:15 +0200

    frame

--- ex1.py
+++ ex1.py
@@ -11,6 +11,19 @@ YELLOW = 40
 GREEN = 50
 PACMAN = 77
 
+WALL = 99
+EATEN = 88
+EMPTY = 10
+PILL = 11
+# pill adds 1 to ghost or empty
+
+RIGHT = "R"
+LEFT = "L"
+UP = "U"
+DOWN = "D"
+
+MOVES = [RIGHT, DOWN, LEFT, UP]
+
 
 def tuple_state_to_list(tuple_state):
     return (list(row) for row in tuple_state)
@@ -29,6 +42,8 @@ class PacmanProblem(search.Problem):
 
         self.locations = dict.fromkeys((7, 2, 3, 4, 5))
         self.dead_end = False
+        self.state_rows = len(initial)
+        self.state_cols = len(initial[0])
 
         """ Constructor only needs the initial state.
         Don't forget to set the goal or implement the goal test"""
@@ -36,15 +51,36 @@ class PacmanProblem(search.Problem):
 
     def successor(self, state):
         """ Generates the successor state """
-        utils.raiseNotDefined()
+        possible_moves = []
+        for move in MOVES:
+            if self.result(state, move) is not None:
+                possible_moves.append(move)
+        return possible_moves
+#       utils.raiseNotDefined()
+
+    def move_packman(self, state, move):
+        pass
+
+    def move_ghosts(self, state):
+        pass
+
 
     def result(self, state, move):
         """given state and an action and return a new state"""
-        utils.raiseNotDefined()
+        list_state = tuple_state_to_list(state)
+        self.move_packman(state, move)
+        self.move_ghosts(state)
+        return list_state_to_tuple(list_state)
+#       utils.raiseNotDefined()
 
     def goal_test(self, state):
         """ given a state, checks if this is the goal state, compares to the created goal state"""
-        utils.raiseNotDefined()
+        for row in state:
+            for item in row:
+                if item == PILL:
+                    return False
+        return True
+#        utils.raiseNotDefined()
 
     def h(self, node):
         """ This is the heuristic. It get a node (not a state)
