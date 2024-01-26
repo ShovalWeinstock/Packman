c62dc1d8509760cff7bb4ebf97c94900a669441c
ShovalWeinstock <92527489+ShovalWeinstock@users.noreply.github.com>
2024-01-26 17:30:15 +0200

    successor

--- ex1.py
+++ ex1.py
@@ -2,7 +2,9 @@ import search
 import math
 import utils
 
-id = "No numbers - I'm special!"
+id = "209540731"
+
+# todo what is an invalid action? eaten? pacman go to wall/end of screen?
 
 """ Rules """
 RED = 20
@@ -21,6 +23,7 @@ RIGHT = "R"
 LEFT = "L"
 UP = "U"
 DOWN = "D"
+STAY = "S"
 
 MOVES = [RIGHT, DOWN, LEFT, UP]
 
@@ -33,6 +36,10 @@ def list_state_to_tuple(list_state):
     return tuple(tuple(row) for row in list_state)
 
 
+def manhattan_distance(point1, point2):
+    return sum(abs(val1-val2) for val1, val2 in zip(point1, point2))
+
+
 class PacmanProblem(search.Problem):
     """This class implements a pacman problem"""
 
@@ -42,8 +49,8 @@ class PacmanProblem(search.Problem):
 
         self.locations = dict.fromkeys((7, 2, 3, 4, 5))
         self.dead_end = False
-        self.state_rows = len(initial)
-        self.state_cols = len(initial[0])
+        self.state_n_rows = len(initial)
+        self.state_n_cols = len(initial[0])
 
         """ Constructor only needs the initial state.
         Don't forget to set the goal or implement the goal test"""
@@ -55,37 +62,68 @@ class PacmanProblem(search.Problem):
         for move in MOVES:
             move_result = self.result(state, move)
             if move_result is not None:
-                possible_moves.append(tuple(move, move_result))
+                possible_moves.append((move, move_result))
         return tuple(possible_moves)
 #       utils.raiseNotDefined()
 
-    def move_packman(self, state, move):
-        pass
+    def get_character_location(self, state, character):
+        for row_i in range(self.state_n_rows):
+            for col_i in range(self.state_n_cols):
+                if state[row_i][col_i] == character:
+                    return [row_i, col_i]
+        return None
+
+    def move_pacman(self, state, move):
+        # get pacman's old location
+        pacman_old_location = self.get_character_location(state, PACMAN)
+        if pacman_old_location is None:
+            return
+        pacman_old_row = pacman_old_location[0]
+        pacman_old_col = pacman_old_location[1]
+
+        # move pacman according to the given move
+        pacman_new_row = pacman_old_row
+        pacman_new_col = pacman_old_col
+        if move == UP:
+            pass #todo fill (if it is wall / end of board - illegal action or no move?)
+        elif move == DOWN:
+            pass #todo fill
+        elif move == RIGHT:
+            pass #todo fill
+        elif move == LEFT:
+            pass #todo fill
+        state[pacman_new_row][pacman_new_col] = PACMAN
+        state[pacman_old_row][pacman_old_col] = EMPTY
+        # 7 is magic number for pacman
+        self.locations[7] = [pacman_new_row, pacman_new_col]
 
     def move_ghosts(self, state):
-        pass
+        pass #todo fill
 
     def result(self, state, move):
         """given state and an action and return a new state"""
+        self.locations = dict.fromkeys((7, 2, 3, 4, 5))
+        self.dead_end = False
         list_state = tuple_state_to_list(state)
-        self.move_packman(state, move)
-        self.move_ghosts(state)
+        self.move_pacman(list_state, move)
+        self.move_ghosts(list_state)
+        if self.dead_end:
+            return None
         return list_state_to_tuple(list_state)
 #       utils.raiseNotDefined()
 
     def goal_test(self, state):
         """ given a state, checks if this is the goal state, compares to the created goal state"""
         for row in state:
-            for item in row:
-                if item == PILL:
-                    return False
+            if PILL in row:
+                return False
         return True
-#        utils.raiseNotDefined()
+#       utils.raiseNotDefined()
 
     def h(self, node):
         """ This is the heuristic. It get a node (not a state)
         and returns a goal distance estimate"""
-        utils.raiseNotDefined()
+        utils.raiseNotDefined() #todo fill
 
 
 def create_pacman_problem(game):
